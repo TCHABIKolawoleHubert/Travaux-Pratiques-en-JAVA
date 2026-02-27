39f49ffc16fc080d246cda9501b0aa7e6ab30e20
TCHABIKolawoleHubert <kolawolehubertt@gmail.com>
2026-02-27 14:28:25 +0100

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,11 @@
+public class addition {
+    public static void main(String[] args) {
+
+        int nombre1 = 10;
+        int nombre2 = 5;
+
+        int somme = nombre1 + nombre2;
+
+        System.out.println("La somme est : " + somme);
+    }
+}
